1becbb1f1742fe1b361aa49fa077ae931f5bb3a2
space doge <99536044+d0g3bl2@users.noreply.github.com>
2022-12-11 15:36:41 +0000

    add icon change function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
     
     const colourButton = document.querySelector('#colour-button');
+    const iconButton = document.querySelector('#icon-button');
     const colourSelectorContainer = document.querySelector('.colour-selector-container-closed');
+    const iconSelectorContainer = document.querySelector('.icon-selector-container-closed');
     const colourInputs = document.querySelectorAll('.colour-inputs');
+    const iconInputs = document.querySelectorAll('.icon-inputs');
     const iconContainer = document.querySelector('#icon-container');
+    const gridPickerIcon = document.querySelector('#grid-picker-icon');
 
 
 
@@ -20,13 +24,37 @@
             colourSelectorContainer.classList.add('colour-selector-container-closed');
         }
     });
+    
+    iconButton.addEventListener('click', (e) => {
+        console.log(e.currentTarget);
+        console.log(iconSelectorContainer.classList);
 
-    function changeBackgroundColour (e) {
+        if (iconSelectorContainer.className === ('icon-selector-container-closed')) {
+            console.log('closed');
+            iconSelectorContainer.classList.remove('icon-selector-container-closed');
+            iconSelectorContainer.classList.add('icon-selector-container-open');
+        } else if (iconSelectorContainer.className === 'icon-selector-container-open') {
+            console.log('open');
+            iconSelectorContainer.classList.remove('icon-selector-container-open');
+            iconSelectorContainer.classList.add('icon-selector-container-closed');
+        }
+    });
+
+    function changePickerGridBackground (e) {
             iconContainer.style.background = e.currentTarget.value;
     }
 
     colourInputs.forEach(input => {
-        input.addEventListener('input', changeBackgroundColour)
+        input.addEventListener('input', changePickerGridBackground)
+    });
+
+    function changePickerGridIcon (e) {
+            // iconContainer.style.background = e.currentTarget.value;
+            gridPickerIcon.src = e.currentTarget.src;
+    }
+
+    iconInputs.forEach(input => {
+        input.addEventListener('click', changePickerGridIcon)
     });
 
             
